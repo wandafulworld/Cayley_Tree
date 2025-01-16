d8735278e50b7f69c6c9fd015dac36773cc8fc14
wandafulworld <wanda.duss@outlook.com>
2025-01-16 14:56:00 +0100

    Update BerryPhaseCalculator.py
    Fixed some errors (conjugation and conversion of the phase)

--- BerryPhaseCalculator.py
+++ BerryPhaseCalculator.py
@@ -21,8 +21,8 @@ def bulk_hamiltonian(k,J1=1,J2=1):
 
 
 def ssh_hamiltonian(k,w=2,v=1): # Top. non trivial phase
-    return np.array([[0, v + w*np.exp(-1j*k)],
-                     [v + w*np.exp(1j*k),0]])
+    return np.array([[0, v + w*np.exp(1j*k)],
+                     [v + w*np.exp(-1j*k),0]])
 
 
 
@@ -30,7 +30,7 @@ def spectrum_calculator(hamiltonian,**kwargs):
     """
     Numerically calculates the spectrum for a given bulk hamiltonian, by scanning thorugh k-space [0,2pi] and calculating
     eigenvalues for each k-value.
-    :param hamiltonian: A bulk hamiltonian that depends on k and J1,J2
+    :param hamiltonian: A bulk hamiltonian that depends on k(!)
     :param *args: Allows to pass any other arguments that are fed into the hamiltonian (positionally)
     :return: The discretized spectrum of the bulk hamiltonian
     """
@@ -63,24 +63,24 @@ def specific_band_projector_calculator(hamiltonian,k,eval_min,eval_max,vector=Fa
     if vector:
         return band_vector
 
-    projector = np.outer(band_vector, band_vector)
-    # print('Froebenius NOrm of projector: ',np.linalg.norm(projector))
+    projector = np.outer(band_vector, np.conjugate(band_vector))
+
     return projector
 
 
 
 def berry_phase_calculator(hamiltonian,eval_min,eval_max,**kwargs):
     #N = 100
-    k_path = np.arange(0, 2 * 3.14, 0.02)
+    k_path = np.arange(0, 2 * np.pi, 0.0001)
     N = len(k_path)
     path_ordered_arrays = []
     for i,k in enumerate(k_path):
         #print(k)
-        if i == 0:
+        if i == 0: # First state is just the vector
             path_ordered_arrays.append(specific_band_projector_calculator(hamiltonian,k,eval_min,eval_max,True,**kwargs))
-        elif i == N-1:
-            path_ordered_arrays.append(specific_band_projector_calculator(hamiltonian,k,eval_min,eval_max,True,**kwargs))
-        else:
+        elif i == N-1: # Last state is just the vector (conjugated)
+            path_ordered_arrays.append(np.conjugate(specific_band_projector_calculator(hamiltonian,k,eval_min,eval_max,True,**kwargs)))
+        else: # Projectors
             path_ordered_arrays.append(specific_band_projector_calculator(hamiltonian,k,eval_min,eval_max,False,**kwargs))
 
     propagating_vector = path_ordered_arrays[0]
@@ -90,8 +90,10 @@ def berry_phase_calculator(hamiltonian,eval_min,eval_max,**kwargs):
 
     for element in path_ordered_arrays[1:]:
         propagating_vector = np.matmul(element,propagating_vector)
+        print(propagating_vector)
 
-    return propagating_vector
+    #return np.angle(propagating_vector) # returns the berry phase
+    return -1j/(2*np.pi)*np.log(propagating_vector) # returns the polarization
 
 
 
@@ -99,37 +101,42 @@ if __name__ == "__main__":
 
     #print('Projector Result: ',specific_band_projector_calculator(bulk_hamiltonian,0.06346651825433926,2,10,vector=False))
     print('-----------------------------------------------')
-    print('Berry-Phase of 2xLC: ',berry_phase_calculator(bulk_hamiltonian,2,10))
+    print('Berry-Phase of 2xLC (Negative band): ',berry_phase_calculator(bulk_hamiltonian,-10,-2))
+    print('Berry-Phase of 2xLC (Middle band): ',berry_phase_calculator(bulk_hamiltonian,-2,2))
+    print('Berry-Phase of 2xLC (Positive band): ',berry_phase_calculator(bulk_hamiltonian,2,10))
     print('-----------------------------------------------')
-    print('Berry-Phase of SSH: ', berry_phase_calculator(ssh_hamiltonian, 0.5, 3.5,w=2,v=1))
+    w = 0
+    v = 1
+    print('Berry-Phase of positive band, SSH:  ', berry_phase_calculator(ssh_hamiltonian, 0.5, 3.5,w=w,v=v))
+    print('Berry-Phase of negative band, SSH:  ', berry_phase_calculator(ssh_hamiltonian, -3.5, -0.2,w=w,v=v))
     print('-----------------------------------------------')
     # Plot Spectra
     fig, ax_list = plt.subplots(2, 1, sharex=True)
     fig.figsize = (10, 15)
 
-    # J2_list = [1,0.25]
-    # fig.suptitle('Bulk Spectrum of 2xLC-Tree, Case 1')
-    #
-    # for i, J2 in enumerate(J2_list):
-    #     spectrum = spectrum_calculator(bulk_hamiltonian,1,J2)
-    #
-    #     ax_list[i].set_title('Strong Bond = ' + str(J2))
-    #     ax_list[i].hist(spectrum, bins=200)
-    #     ax_list[i].set_ylabel('Counts')
-    #
-    # ax_list[-1].set_xlabel('E/t')
+    J2_list = [1,0.25]
+    fig.suptitle('Bulk Spectrum of 2xLC-Tree, Case 1')
 
-    w_list = [2,1]
-    fig.suptitle('Bulk Spectrum of SSH Model')
+    for i, J2 in enumerate(J2_list):
+        spectrum = spectrum_calculator(bulk_hamiltonian,J1=1,J2=J2)
 
-    for i, w in enumerate(w_list):
-        spectrum = spectrum_calculator(ssh_hamiltonian,w=w)
-
-        ax_list[i].set_title('W = ' + str(w))
+        ax_list[i].set_title('Strong Bond = ' + str(J2))
         ax_list[i].hist(spectrum, bins=200)
         ax_list[i].set_ylabel('Counts')
 
     ax_list[-1].set_xlabel('E/t')
 
-    plt.tight_layout()
+    # w_list = [2,0.5]
+    # fig.suptitle('Bulk Spectrum of SSH Model')
+    #
+    # for i, w in enumerate(w_list):
+    #     spectrum = spectrum_calculator(ssh_hamiltonian,w=w,v=1)
+    #
+    #     ax_list[i].set_title('W = ' + str(w))
+    #     ax_list[i].hist(spectrum, bins=200)
+    #     ax_list[i].set_ylabel('Counts')
+    #
+    # ax_list[-1].set_xlabel('E/t')
+    #
+    # plt.tight_layout()
     plt.show()
